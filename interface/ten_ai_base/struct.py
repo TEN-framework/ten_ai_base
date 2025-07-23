38884917747058326ce337554dc91b2eb18c3b29
sunxilin <sunxilin@shengwang.cn>
2025-07-23 16:10:15 +0800

    feat: refine asr base class

--- interface/ten_ai_base/struct.py
+++ interface/ten_ai_base/struct.py
@@ -1,16 +1,19 @@
 from typing import Any
 from pydantic import BaseModel
 
+
 class TTSWord(BaseModel):
     word: str = ""
     start_ms: int = -1  # start time of the word, milliseconds since epoch
     duration_ms: int = -1  # duration of the word, in milliseconds
+
 
 class TTSTextInput(BaseModel):
     request_id: str
     text: str
     text_input_end: bool = False
     metadata: dict[str, Any] = {}  # additional metadata for the transcription
+
 
 class TTSTextResult(BaseModel):
     request_id: str
@@ -21,6 +24,25 @@ class TTSTextResult(BaseModel):
     text_result_end: bool = False
     metadata: dict[str, Any] = {}  # additional metadata for the transcription
 
+
 class TTSFlush(BaseModel):
     flush_id: str
     metadata: dict[str, Any] = {}  # additional metadata for the flush operation
+
+
+class ASRWord(BaseModel):
+    word: str
+    start_ms: int
+    duration_ms: int
+    stable: bool
+
+
+class ASRResult(BaseModel):
+    id: str
+    text: str
+    final: bool
+    start_ms: int
+    duration_ms: int
+    language: str
+    words: list[ASRWord] | None = None
+    metadata: dict[str, Any] = {}  # additional metadata for the transcription

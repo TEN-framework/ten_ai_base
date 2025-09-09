200acc0d9b1d0052b728e3a6e8dbb3dd16fd9a68
YiminW <wym0207@outlook.com>
2025-09-09 23:08:28 +0800

    feat: add log in base tts2

--- interface/ten_ai_base/tts2.py
+++ interface/ten_ai_base/tts2.py
@@ -10,7 +10,13 @@ import traceback
 import uuid
 
 from .helper import AsyncQueue
-from .message import ModuleError, ModuleMetricKey, ModuleMetrics, ModuleType, TTSAudioEndReason
+from .message import (
+    ModuleError,
+    ModuleMetricKey,
+    ModuleMetrics,
+    ModuleType,
+    TTSAudioEndReason,
+)
 
 from .struct import TTSFlush, TTSTextInput, TTSTextResult
 from ten_runtime import (
@@ -40,7 +46,7 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
     # Create the queue for message processing
     def __init__(self, name: str):
         super().__init__(name)
-        self.ten_env: AsyncTenEnv = None # type: ignore
+        self.ten_env: AsyncTenEnv = None  # type: ignore
         self.input_queue = AsyncQueue()
         self.current_task = None
         self.loop_task = None
@@ -52,7 +58,7 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
         self.input_characters = 0
         self.recv_audio_duration = 0
         self.recv_audio_chunks = b""
-        #billing metricstotal
+        # billing metricstotal
         self.total_output_characters = 0
         self.total_input_characters = 0
         self.total_recv_audio_duration = 0
@@ -69,14 +75,13 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
             self.loop_task = self.loop.create_task(self._process_input_queue(ten_env))
 
     async def on_stop(self, ten_env: AsyncTenEnv) -> None:
-        #send laster time before stop
-        await self.send_billing_metrics()
+        # send laster time before stop
+        await self.send_metrics()
         await super().on_stop(ten_env)
         await self._flush_input_items()
         if self.loop_task:
             self.loop_task.cancel()
         await self.input_queue.put(None)  # Signal the loop to stop processing
-
 
     async def on_deinit(self, ten_env: AsyncTenEnv) -> None:
         await super().on_deinit(ten_env)
@@ -98,43 +103,47 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
             data_payload, err = data.get_property_to_json("")
             if err:
                 raise RuntimeError(f"Failed to get data payload: {err}")
-            ten_env.log_debug(
-                f"on_data {data_name}, payload {data_payload}"
-            )
+            ten_env.log_debug(f"on_data {data_name}, payload {data_payload}")
 
             try:
                 t = TTSTextInput.model_validate_json(data_payload)
             except Exception as e:
-                ten_env.log_warn(
-                    f"invalid data {data_name} payload, err {e}"
-                )
+                ten_env.log_warn(f"invalid data {data_name} payload, err {e}")
                 return
-
+            self.ten_env.log_info(
+                f"get tts_text_input:  {data_payload}",
+                category=LOG_CATEGORY_KEY_POINT,
+            )
             # Start an asynchronous task for handling tts
             await self.input_queue.put(t)
         if data.get_name() == DATA_FLUSH:
             data_payload, err = data.get_property_to_json("")
             if err:
                 raise RuntimeError(f"Failed to get data payload: {err}")
-            ten_env.log_debug(
-                f"on_data {data_name}, payload {data_payload}"
-            )
+            ten_env.log_debug(f"on_data {data_name}, payload {data_payload}")
 
             try:
                 t = TTSFlush.model_validate_json(data_payload)
             except Exception as e:
-                ten_env.log_warn(
-                    f"invalid data {data_name} payload, err {e}"
-                )
+                ten_env.log_warn(f"invalid data {data_name} payload, err {e}")
                 return
-
+            ten_env.log_info(
+                f"receive tts_flush {data_payload} ",
+                category=LOG_CATEGORY_KEY_POINT,
+            )
             await self._flush_input_items()
             flush_result = Data.create(DATA_FLUSH_RESULT)
-            flush_result.set_property_from_json(
-                None, json.dumps({
+            json_data = json.dumps(
+                {
                     "flush_id": t.flush_id,
                     "metadata": t.metadata,
-                })
+                }
+            )
+            flush_result.set_property_from_json(None, json_data)
+
+            ten_env.log_info(
+                f"send tts_flush_end {json_data}",
+                category=LOG_CATEGORY_KEY_POINT,
             )
             await ten_env.send_data(flush_result)
             ten_env.log_debug("on_data sent flush result")
@@ -147,14 +156,12 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
         # Cancel the current task if one is running
         await self._cancel_current_task()
 
-
     async def _cancel_current_task(self) -> None:
         """Called when the TTS request is cancelled."""
         if self.current_task:
             self.current_task.cancel()
             self.current_task = None
         self.leftover_bytes = b""
-
 
     async def _process_input_queue(self, ten_env: AsyncTenEnv) -> None:
         """Asynchronously process queue items one by one."""
@@ -173,10 +180,7 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
                     f"Task failed: {t.text}, err: {traceback.format_exc()}"
                 )
 
-
-    async def send_tts_audio_data(
-        self, audio_data: bytes, timestamp: int = 0
-    ) -> None:
+    async def send_tts_audio_data(self, audio_data: bytes, timestamp: int = 0) -> None:
         """End sending audio out."""
         try:
             sample_rate = self.synthesize_audio_sample_rate()
@@ -186,10 +190,7 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
             combined_data = self.leftover_bytes + audio_data
 
             # Check if combined_data length is odd
-            if (
-                len(combined_data) % (bytes_per_sample * number_of_channels)
-                != 0
-            ):
+            if len(combined_data) % (bytes_per_sample * number_of_channels) != 0:
                 # Save the last incomplete frame
                 valid_length = len(combined_data) - (
                     len(combined_data) % (bytes_per_sample * number_of_channels)
@@ -206,8 +207,7 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
                 f.set_number_of_channels(number_of_channels)
                 f.set_data_fmt(AudioFrameDataFmt.INTERLEAVE)
                 f.set_samples_per_channel(
-                    len(combined_data)
-                    // (bytes_per_sample * number_of_channels)
+                    len(combined_data) // (bytes_per_sample * number_of_channels)
                 )
                 f.alloc_buf(len(combined_data))
                 f.set_timestamp(timestamp)
@@ -216,70 +216,81 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
                 f.unlock_buf(buff)
                 await self.ten_env.send_audio_frame(f)
         except Exception as e:
-            self.ten_env.log_error(
-                f"error send audio frame, {traceback.format_exc()}"
-            )
+            self.ten_env.log_error(f"error send audio frame, {traceback.format_exc()}")
 
-    async def send_tts_text_result(
-        self, t: TTSTextResult
-    ) -> None:
+    async def send_tts_text_result(self, t: TTSTextResult) -> None:
         data = Data.create(DATA_TTS_TEXT_RESULT)
         data.set_property_from_json("", t.model_dump_json())
         await self.ten_env.send_data(data)
 
-
     async def send_tts_ttfb_metrics(
-            self, request_id: str, ttfb_ms: int, turn_id: int = -1
+        self, request_id: str, ttfb_ms: int, turn_id: int = -1
     ) -> None:
         data = Data.create("metrics")
         metrics = ModuleMetrics(
             id=request_id,
             module=ModuleType.TTS,
             vendor=self.vendor(),
-            metrics={
-                ModuleMetricKey.TTS_TTFB: ttfb_ms
-            },
+            metrics={ModuleMetricKey.TTS_TTFB: ttfb_ms},
             metadata={
                 "session_id": self.session_id or "",
                 "turn_id": turn_id,
             },
         )
+        self.ten_env.log_info(
+            f"tts_ttfb:  {metrics} of request_id: {request_id}",
+            category=LOG_CATEGORY_KEY_POINT,
+        )
         data.set_property_from_json(None, metrics.model_dump_json())
         await self.ten_env.send_data(data)
 
-
-    async def send_tts_audio_start(
-            self, request_id: str, turn_id: int = -1
-    ) -> None:
+    async def send_tts_audio_start(self, request_id: str, turn_id: int = -1) -> None:
         data = Data.create("tts_audio_start")
-        data.set_property_from_json(None, json.dumps({
-            "request_id": request_id,
-            "metadata": {
-                "session_id": self.session_id or "",
-                "turn_id": turn_id,
+        json_data = json.dumps(
+            {
+                "request_id": request_id,
+                "metadata": {
+                    "session_id": self.session_id or "",
+                    "turn_id": turn_id,
+                },
             }
-        }))
+        )
+        data.set_property_from_json(None, json_data)
+        self.ten_env.log_info(
+            f"tts_audio_start:  {json_data} of request_id: {request_id}",
+            category=LOG_CATEGORY_KEY_POINT,
+        )
         await self.ten_env.send_data(data)
 
     async def send_tts_audio_end(
-            self, request_id: str, request_event_interval_ms: int, request_total_audio_duration_ms: int, turn_id: int = -1, reason: TTSAudioEndReason = TTSAudioEndReason.REQUEST_END
+        self,
+        request_id: str,
+        request_event_interval_ms: int,
+        request_total_audio_duration_ms: int,
+        turn_id: int = -1,
+        reason: TTSAudioEndReason = TTSAudioEndReason.REQUEST_END,
     ) -> None:
         data = Data.create("tts_audio_end")
-        data.set_property_from_json(None, json.dumps({
-            "request_id": request_id,
-            "request_event_interval_ms": request_event_interval_ms,
-            "request_total_audio_duration_ms": request_total_audio_duration_ms,
-            "reason": reason.value,
-            "metadata": {
-                "session_id": self.session_id or "",
-                "turn_id": turn_id,
+        json_data = json.dumps(
+            {
+                "request_id": request_id,
+                "request_event_interval_ms": request_event_interval_ms,
+                "request_total_audio_duration_ms": request_total_audio_duration_ms,
+                "reason": reason.value,
+                "metadata": {
+                    "session_id": self.session_id or "",
+                    "turn_id": turn_id,
+                },
             }
-        }))
+        )
+        data.set_property_from_json(None, json_data)
+        self.ten_env.log_info(
+            f"tts_audio_end:  {json_data} of request_id: {request_id}",
+            category=LOG_CATEGORY_KEY_POINT,
+        )
         await self.ten_env.send_data(data)
 
-    async def send_tts_error(
-        self, request_id: str | None, error: ModuleError
-    ) -> None:
+    async def send_tts_error(self, request_id: str | None, error: ModuleError) -> None:
         """
         Send an error message related to ASR processing.
         """
@@ -293,24 +304,28 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
                 "code": vendor_info.code,
                 "message": vendor_info.message,
             }
-
+        json_data = json.dumps(
+            {
+                "id": request_id or "",
+                "code": error.code,
+                "message": error.message,
+                "vendor_info": vendorInfo or {},
+                "metadata": {"session_id": self.session_id or ""},
+            }
+        )
         error_data.set_property_from_json(
             None,
-            json.dumps(
-                {
-                    "id": request_id or "",
-                    "code": error.code,
-                    "message": error.message,
-                    "vendor_info": vendorInfo or {},
-                    "metadata": {"session_id": self.session_id or ""},
-                }
-            ),
+            json_data,
         )
-
+        self.ten_env.log_error(
+            f"tts_error:  msg: 	{json_data}",
+            category=LOG_CATEGORY_KEY_POINT,
+        )
         await self.ten_env.send_data(error_data)
-    #send when tts audio end
-    async def send_billing_metrics(self):
-        await self.billing_metrics_calculate_duration()
+
+    # send when tts audio end
+    async def send_metrics(self, request_id: str = ""):
+        await self.metrics_calculate_duration()
         data = Data.create("metrics")
         metrics = ModuleMetrics(
             id=self.get_uuid(),
@@ -323,30 +338,45 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
                 "total_output_characters": self.total_output_characters,
                 "total_input_characters": self.total_input_characters,
                 "total_recv_audio_duration": self.total_recv_audio_duration,
-            }
+            },
         )
-        self.ten_env.log_debug(f"billing_metrics: {metrics}")
+        self.ten_env.log_debug(f"metrics: {metrics}")
+
+        self.ten_env.log_info(
+            f"tts_metrics:  {metrics} of request_id: {request_id}",
+            category=LOG_CATEGORY_KEY_POINT,
+        )
         data.set_property_from_json(None, metrics.model_dump_json())
         await self.ten_env.send_data(data)
-        self.billing_metrics_reset()
+        self.metrics_reset()
 
-    async def billing_metrics_calculate_duration(self) -> None:
-        self.recv_audio_duration = (float(len(self.recv_audio_chunks)) / self.synthesize_audio_channels() * 1000 / self.synthesize_audio_sample_width()) / self.synthesize_audio_sample_rate()
-        self.total_recv_audio_duration = (float(len(self.total_recv_audio_chunks)) / self.synthesize_audio_channels() * 1000 / self.synthesize_audio_sample_width()) / self.synthesize_audio_sample_rate()
+    async def metrics_calculate_duration(self) -> None:
+        self.recv_audio_duration = (
+            float(len(self.recv_audio_chunks))
+            / self.synthesize_audio_channels()
+            * 1000
+            / self.synthesize_audio_sample_width()
+        ) / self.synthesize_audio_sample_rate()
+        self.total_recv_audio_duration = (
+            float(len(self.total_recv_audio_chunks))
+            / self.synthesize_audio_channels()
+            * 1000
+            / self.synthesize_audio_sample_width()
+        ) / self.synthesize_audio_sample_rate()
 
-    def billing_metrics_add_output_characters(self, characters: int) -> None:
+    def metrics_add_output_characters(self, characters: int) -> None:
         self.output_characters += characters
         self.total_output_characters += characters
 
-    def billing_metrics_add_input_characters(self, characters: int) -> None:
+    def metrics_add_input_characters(self, characters: int) -> None:
         self.input_characters += characters
         self.total_input_characters += characters
 
-    def billing_metrics_add_recv_audio_chunks(self, chunks: bytes) -> None:
+    def metrics_add_recv_audio_chunks(self, chunks: bytes) -> None:
         self.total_recv_audio_chunks += chunks
         self.recv_audio_chunks += chunks
 
-    def billing_metrics_reset(self) -> None:
+    def metrics_reset(self) -> None:
         self.output_characters = 0
         self.input_characters = 0
         self.recv_audio_duration = 0
@@ -360,12 +390,11 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
             vendor=self.vendor(),
             metrics={
                 "connect_delay": connect_delay_ms,
-            }
+            },
         )
         self.ten_env.log_debug(f"metrics_connect_delay: {metrics}")
         data.set_property_from_json(None, metrics.model_dump_json())
         await self.ten_env.send_data(data)
-
 
     @abstractmethod
     def vendor(self) -> str:
@@ -373,30 +402,22 @@ class AsyncTTS2BaseExtension(AsyncExtension, ABC):
         Get the vendor name of the TTS implementation.
         This is used for metrics and error reporting.
         """
-        raise NotImplementedError(
-            "This method should be implemented in subclasses."
-        )
+        raise NotImplementedError("This method should be implemented in subclasses.")
 
     @abstractmethod
-    async def request_tts(
-        self, t: TTSTextInput
-    ) -> None:
+    async def request_tts(self, t: TTSTextInput) -> None:
         """
         Called when a new input item is available in the queue. Override this method to implement the TTS request logic.
         Use send_audio_out to send the audio data to the output when the audio data is ready.
         """
-        raise NotImplementedError(
-            "request_tts must be implemented in the subclass"
-        )
+        raise NotImplementedError("request_tts must be implemented in the subclass")
 
     @abstractmethod
     def synthesize_audio_sample_rate(self) -> int:
         """
         Get the input audio sample rate in Hz.
         """
-        raise NotImplementedError(
-            "This method should be implemented in subclasses."
-        )
+        raise NotImplementedError("This method should be implemented in subclasses.")
 
     def synthesize_audio_channels(self) -> int:
         """
